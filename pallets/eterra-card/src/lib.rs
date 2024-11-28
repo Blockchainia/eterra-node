e0f5ea1dd5ef1079bb28f06165424adf5858b932
Ed Anderson <36516131+eca20@users.noreply.github.com>
2024-11-27 21:16:08 -0500

    remove deprecated code

--- pallets/eterra-card/src/lib.rs
+++ pallets/eterra-card/src/lib.rs
@@ -4,82 +4,71 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::{
-        pallet_prelude::*,
-        traits::{Currency, ReservableCurrency},
-        Blake2_128Concat,
-        dispatch::Weight,
-    };
-    use frame_system::pallet_prelude::*;
-    use scale_info::TypeInfo;
-    use sp_runtime::traits::Hash;
+	use frame_support::{
+		dispatch::Weight,
+		pallet_prelude::*,
+		traits::{Currency, ReservableCurrency},
+		Blake2_128Concat,
+	};
+	use frame_system::pallet_prelude::*;
+	use scale_info::TypeInfo;
+	use sp_runtime::traits::Hash;
 
-    #[pallet::pallet]
-    #[pallet::generate_store(pub(super) trait Store)]
-    pub struct Pallet<T>(_);
+	#[pallet::pallet]
+	#[pallet::without_storage_info] // Recommended to avoid unnecessary storage info generation
+	pub struct Pallet<T>(_);
 
-    #[pallet::config]
-    pub trait Config: frame_system::Config {
-        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-        type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
-    }
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+		type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
+	}
 
-    #[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, TypeInfo, MaxEncodedLen)]
-    pub struct Card {
-        pub level: u32,
-        pub hp: u32,
-        pub mp: u32,
-    }
+	#[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, TypeInfo, MaxEncodedLen)]
+	pub struct Card {
+		pub level: u32,
+		pub hp: u32,
+		pub mp: u32,
+	}
 
-    #[pallet::storage]
-    #[pallet::getter(fn cards)]
-    pub type Cards<T: Config> = StorageMap<
-        _,
-        Blake2_128Concat,
-        T::Hash,
-        Card,
-        OptionQuery,
-    >;
+	#[pallet::storage]
+	#[pallet::getter(fn cards)]
+	pub type Cards<T: Config> = StorageMap<_, Blake2_128Concat, T::Hash, Card, OptionQuery>;
 
-    #[pallet::storage]
-    #[pallet::getter(fn owner_of)]
-    pub type OwnerOf<T: Config> = StorageMap<
-        _,
-        Blake2_128Concat,
-        T::Hash,
-        T::AccountId,
-        OptionQuery,
-    >;
+	#[pallet::storage]
+	#[pallet::getter(fn owner_of)]
+	pub type OwnerOf<T: Config> =
+		StorageMap<_, Blake2_128Concat, T::Hash, T::AccountId, OptionQuery>;
 
-    #[pallet::event]
-    #[pallet::generate_deposit(pub(super) fn deposit_event)]
-    pub enum Event<T: Config> {
-        CardCreated { card_id: T::Hash, owner: T::AccountId },
-    }
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		CardCreated { card_id: T::Hash, owner: T::AccountId },
+	}
 
-    #[pallet::error]
-    pub enum Error<T> {
-        CardAlreadyExists,
-    }
+	#[pallet::error]
+	pub enum Error<T> {
+		CardAlreadyExists,
+	}
 
-    #[pallet::call]
-    impl<T: Config> Pallet<T> {
-        /// Create a new card.
-        #[pallet::call_index(0)]
-        #[pallet::weight(Weight::from_parts(10_000, 0))] // Explicit weight type
-        pub fn create_card(origin: OriginFor<T>, stats: Card) -> DispatchResult {
-            let who = ensure_signed(origin)?;
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// Create a new card.
+		#[pallet::call_index(0)]
+		#[pallet::weight(Weight::from_parts(10_000, 0))] // Explicit weight type
+		pub fn create_card(origin: OriginFor<T>, stats: Card) -> DispatchResult {
+			let who = ensure_signed(origin)?;
 
-            let card_id = T::Hashing::hash_of(&stats);
+			let card_id = T::Hashing::hash_of(&stats);
 
-            ensure!(!Cards::<T>::contains_key(&card_id), Error::<T>::CardAlreadyExists);
+			ensure!(!Cards::<T>::contains_key(&card_id), Error::<T>::CardAlreadyExists);
 
-            Cards::<T>::insert(&card_id, stats);
-            OwnerOf::<T>::insert(&card_id, &who);
+			Cards::<T>::insert(&card_id, stats);
+			OwnerOf::<T>::insert(&card_id, &who);
 
-            Self::deposit_event(Event::CardCreated { card_id, owner: who });
+			Self::deposit_event(Event::CardCreated { card_id, owner: who });
 
-            Ok(())
-        }
-    }
+			Ok(())
+		}
+	}
 }
